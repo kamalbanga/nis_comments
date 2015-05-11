c621f9572950b64d2723cc1fc38dabfce68baa54
Kamal Banga <banga.kamal@gmail.com>
2015-05-11 23:26:07 +0530

    Change database config for beanstalk

--- nis_comments/settings.py
+++ nis_comments/settings.py
@@ -79,16 +79,29 @@ WSGI_APPLICATION = 'nis_comments.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': 'matrix',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ['RDS_DB_NAME'],
+    'USER': os.environ['RDS_USERNAME'],
+    'PASSWORD': os.environ['RDS_PASSWORD'],
+    'HOST': os.environ['RDS_HOSTNAME'],
+    'PORT': os.environ['RDS_PORT'],
     }
-}
+    }
+else:
+    DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'mydb',
+    'USER': 'root',
+    'PASSWORD': 'matrix',
+    'HOST': 'localhost',
+    'PORT': '3306',
+    }
+    }
+    }
 
 
 # Internationalization
