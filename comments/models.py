187a4653e17514dcb030e92b83e5f87e8c70531a
Kamal Banga <banga.kamal@gmail.com>
2015-05-26 19:58:12 +0530

    Add timestamp to comment & vote
    Also add a default uuid calculating function to uuid in comment, so that when a comment is created it automatically creates a uuid for it. So even in a POST request, no manual steps are to be done for inserting uuid in a comment.

--- comments/models.py
+++ comments/models.py
@@ -11,7 +11,7 @@ class News(models.Model):
         return self.news_id
 
 class Comment(models.Model):
-    uuid = models.CharField(max_length=40, default='random')
+    uuid = models.CharField(max_length=40, default=uuid.uuid4)
     news = models.ForeignKey(News)
     user = models.ForeignKey(User)
     text = models.CharField(max_length=300, null=True)
@@ -19,6 +19,8 @@ class Comment(models.Model):
     downvotes = models.PositiveIntegerField(default=0)
     isDeleted = models.BooleanField(default=False)
     votes = models.ManyToManyField(User, through='Vote', related_name='votes_table', default=None)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    last_edit = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __unicode__(self):
         return self.text
@@ -27,3 +29,4 @@ class Vote(models.Model):
     cmt = models.ForeignKey(Comment)
     user = models.ForeignKey(User)
     vote_type = models.SmallIntegerField(default=0) # vote_type is +1 for upvote & -1 for downvote
+    ts = models.DateTimeField(auto_now_add=True, blank=True, null=True)
