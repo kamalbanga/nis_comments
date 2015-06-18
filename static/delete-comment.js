cd252f05ac8e7b8e9fada177837570c5324d221f
Kamal Banga <banga.kamal@gmail.com>
2015-06-18 20:38:21 +0530

    Delete redundant static files folder

--- static/delete-comment.js
+++
@@ -1,5 +0,0 @@
- $(function() {
- 	$('.delete_comment').click(function(){
- 		$.get('/delete/', {uuid: $(this).attr("comment-id")});
- 	});
- });
--- static/vote-ajax.js
+++
@@ -1,8 +0,0 @@
- $(function() {
- 	$('.upvote').click(function(){
- 		$.get('/vote/', {vote: 1, id: $(this).attr("comment-id")});
- 	});
- 	$('.downvote').click(function(){
- 		$.get('/vote/', {vote: -1, id: $(this).attr("comment-id")});
- 	});
- });
